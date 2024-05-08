636002d5aa183cadd3d858f5aa9116b551468ee1
MitchCarothers <carothersmitchell@gmail.com>
2024-05-08 13:28:47 -0500

    Restructured physics object spawning systems

--- physics.js
+++ physics.js
@@ -1,4 +1,3 @@
-// module aliases
 let Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -7,6 +6,7 @@ let Engine = Matter.Engine,
     Mouse = Matter.Mouse,
     MouseConstraint = Matter.MouseConstraint;
 
+// matter js physics engine setup
 let engine = Engine.create();
 
 function determineWindowWidth() {
@@ -15,9 +15,8 @@ function determineWindowWidth() {
 }
 let winWidth = determineWindowWidth();
 
-// create a renderer
 let render = Render.create({
-    element: document.getElementById("main_left"),
+    element: document.getElementById("physics"),
     engine: engine,
     options: {
         width: winWidth,
@@ -38,18 +37,97 @@ const mouseParams = {
       render: {
         visible: false,
       },
-    },
+    }
 };
 let mouseConstraint = MouseConstraint.create(engine, mouseParams);
 
-// disables the mouse constraint when mouse leaves the canvas :)
+// disable the mouse constraint when mouse leaves the canvas :)
 render.canvas.addEventListener("mouseleave", () => {
-    const event = new Event("mouseup")
-    mouseConstraint.mouse.element.dispatchEvent(event)
+    const event = new Event("mouseup");
+    mouseConstraint.mouse.element.dispatchEvent(event);
 });
 
+// object spawning system
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+};
 
-// initialize skill buttons
+function spawnPos () {
+    return getRandomInt((winWidth / 8), (winWidth - (winWidth / 8)));
+};
+
+// this function primarily exists to increase readability when spawning objects
+let spawn = function spawn (param) {
+    try {
+        switch (param.type) {
+            case "rectangle":
+                Composite.add(engine.world, Bodies.rectangle(param.x, param.y, param.width, param.height, { 
+                    render: { 
+                        sprite: { texture: param.texture, xScale: param.xScale, yScale: param.yScale } 
+                    } 
+                }));
+                break;
+            case "circle":
+                Composite.add(engine.world, Bodies.circle(param.x, param.y, param.rad, { 
+                    render: { 
+                        sprite: { texture: param.texture, xScale: param.xScale, yScale: param.yScale } 
+                    } 
+                }));
+                break;
+            case "polygon":
+                Composite.add(engine.world, Bodies.polygon(param.x, param.y, param.sides, param.rad, { 
+                    render: { 
+                        sprite: { texture: param.texture, xScale: param.xScale, yScale: param.yScale } 
+                    } 
+                }));
+                break;
+        };
+    } catch {
+        throw("Invalid spawn");
+    }
+};
+
+// initial spawn
+spawn({type:"rectangle", x:spawnPos(), y:100, width:80, height:112, texture:"/images/html.png", xScale:.055, yScale:.055});
+spawn({type:"rectangle", x:spawnPos(), y:100, width:80, height:112, texture:"/images/css.png", xScale:.14, yScale:.14});
+spawn({type:"rectangle", x:spawnPos(), y:100, width:112, height:112, texture:"/images/js.png", xScale:.509, yScale:.509});
+spawn({type:"circle", x:spawnPos(), y:200, rad:58, texture:"/images/lua.png", xScale:.14, yScale:.14});
+spawn({type:"circle", x:spawnPos(), y:200, rad:58, texture:"/images/py.png", xScale:.53, yScale:.53});
+spawn({type:"rectangle", x:spawnPos(), y:300, width:80, height:112, texture:"/images/bat.png", xScale:.45, yScale:.45});
+spawn({type:"circle", x:spawnPos(), y:300, rad:58, texture:"/images/postgre.png", xScale:.14, yScale:.14});
+spawn({type:"rectangle", x:spawnPos(), y:300, width:85, height:90, texture:"/images/database.png", xScale:.2, yScale:.2});
+spawn({type:"polygon", x:spawnPos(), y:400, sides:6, rad:64, texture:"/images/c_sharp.png", xScale:.06, yScale:.06});
+spawn({type:"rectangle", x:spawnPos(), y:80, width:400, height:20, texture:"/images/pencil.png", xScale:.3, yScale:.3});
+
+// skill button spawns
+function addBody (event) {
+    switch (event.target) {
+        case skillButtons.skillWeb:
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:80, height:112, texture:"/images/html.png", xScale:.055, yScale:.055});
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:80, height:112, texture:"/images/css.png", xScale:.14, yScale:.14});
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:112, height:112, texture:"/images/js.png", xScale:.509, yScale:.509});
+            break;
+        case skillButtons.skillScript:
+            spawn({type:"circle", x:spawnPos(), y:100, rad:58, texture:"/images/lua.png", xScale:.14, yScale:.14});
+            spawn({type:"circle", x:spawnPos(), y:100, rad:58, texture:"/images/py.png", xScale:.53, yScale:.53});
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:80, height:112, texture:"/images/bat.png", xScale:.45, yScale:.45});
+            break;
+        case skillButtons.skillSql:
+            spawn({type:"circle", x:spawnPos(), y:100, rad:58, texture:"/images/postgre.png", xScale:.14, yScale:.14});
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:85, height:90, texture:"/images/database.png", xScale:.2, yScale:.2});
+            break;
+        case skillButtons.skillCSharp:
+            spawn({type:"polygon", x:spawnPos(), y:100, sides:6, rad:64, texture:"/images/c_sharp.png", xScale:.06, yScale:.06});
+            break;
+        case skillButtons.skillWriting:
+            spawn({type:"rectangle", x:spawnPos(), y:100, width:400, height:20, texture:"/images/pencil.png", xScale:.3, yScale:.3});
+            break;
+    }
+}
+
+// set up skill spawn buttons
 let skillButtons = {
     skillWeb: document.getElementById("skill_web"),
     skillScript: document.getElementById("skill_script"),
@@ -64,43 +142,7 @@ for (button in skillButtons) {
     });
 }
 
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-function spawnPos () {
-    return getRandomInt((winWidth / 8), (winWidth - (winWidth / 8)))
-}
-
-function addBody (event) {
-    // I don't like the way this looks but I'm not sure how else to structure this...
-    switch (event.target) {
-        case skillButtons.skillWeb:
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 80, 112, { render: { sprite: { texture: "/images/html.png", xScale: .055, yScale: .055} } }));
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 80, 112, { render: { sprite: { texture: "/images/css.png", xScale: .14, yScale: .14} } }));
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 112, 112, { render: { sprite: { texture: "/images/js.png", xScale: .509, yScale: .509} } }));
-            break;
-        case skillButtons.skillScript:
-            Composite.add(engine.world, Bodies.circle(spawnPos(), 100, 58, { render: { sprite: { texture: "/images/lua.png", xScale: .14, yScale: .14} } }));
-            Composite.add(engine.world, Bodies.circle(spawnPos(), 100, 58, { render: { sprite: { texture: "/images/py.png", xScale: .5, yScale: .5} } }));
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 80, 112, { render: { sprite: { texture: "/images/bat.png", xScale: .45, yScale: .45} } }));
-            break;
-        case skillButtons.skillSql:
-            Composite.add(engine.world, Bodies.circle(spawnPos(), 100, 58, { render: { sprite: { texture: "/images/postgre.png", xScale: .14, yScale: .14} } }));
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 85, 90, { render: { sprite: { texture: "/images/database.png", xScale: .2, yScale: .2} } }));
-            break;
-        case skillButtons.skillCSharp:
-            Composite.add(engine.world, Bodies.polygon(spawnPos(), 100, 6, 64, { render: { sprite: { texture: "/images/c_sharp.png", xScale: .06, yScale: .06} } }))
-            break;
-        case skillButtons.skillWriting:
-            Composite.add(engine.world, Bodies.rectangle(spawnPos(), 100, 400, 20, { render: { sprite: { texture: "/images/pencil.png", xScale: .3, yScale: .3} } }));
-            break;
-    }
-}
-
-// boundaries
+// world boundaries
 let winHeight = render.options.height;
 let boundary = 60;
 // This looks confusing because it is, but at its core, this is just a way to dynamically adjust the boundaries
@@ -109,14 +151,9 @@ let ground = Bodies.rectangle((winWidth / 2), (winHeight + (boundary / 2)), winW
     rightWall = Bodies.rectangle((winWidth + (boundary / 2)), (winHeight / 2), boundary, winHeight, { isStatic: true }),
     ceiling = Bodies.rectangle((winWidth / 2), -(boundary / 2), winWidth, boundary, { isStatic: true });
 
-// add boundaries
 Composite.add(engine.world, [ mouseConstraint, ground, leftWall, rightWall, ceiling ]);
 
-// run the renderer
+// run the renderer, create runner, then run the engine (matter js shenanigans)
 Render.run(render);
-
-// create runner
 let runner = Runner.create();
-
-// run the engine
 Runner.run(runner, engine);
